7540d914b65d48a627211ff017b26ea2e8d7d4bf
JOSE JORGE BAZAGA <61705851+jjbazaga@users.noreply.github.com>
2022-07-02 18:39:35 -0300

    Construção do Formulário Login  Simples

--- SistemaVendas/Controllers/HomeController.cs
+++ SistemaVendas/Controllers/HomeController.cs
@@ -11,9 +11,13 @@ namespace SistemaVendas.Controllers
 {
     public class HomeController : Controller
     {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         public IActionResult Index()
         {
-            
             return View();
         }
 
