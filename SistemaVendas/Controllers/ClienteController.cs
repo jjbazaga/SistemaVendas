f5c8968c0243a0d5516a02815f0da868086a48b7
JOSE JORGE BAZAGA <61705851+jjbazaga@users.noreply.github.com>
2022-07-04 14:32:51 -0300

    Listagem de Clientes

---
+++ SistemaVendas/Controllers/ClienteController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SistemaVendas.Models;
+
+namespace SistemaVendas.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.ListaClientes = new ClienteModel().ListarTodosCliente();
+            return View();
+        }
+    }
+}
---
+++ SistemaVendas/Models/ClienteModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data;
+using SistemaVendas.Uteis;
+
+namespace SistemaVendas.Models
+{
+    public class ClienteModel
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+
+        public List<ClienteModel> ListarTodosCliente()
+        {
+            List<ClienteModel> lista = new List<ClienteModel>();
+            ClienteModel item;
+            DAL objDAL = new DAL();
+            string sql = "SELECT id, nome, cpf_cnpj, email, senha FROM sistema_venda.cliente ORDER by nome ASC";
+            DataTable dt = objDAL.RetDataTable(sql);
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                item = new ClienteModel
+                {
+                    Id = dt.Rows[i]["Id"].ToString(),
+                    Nome = dt.Rows[i]["Nome"].ToString(),
+                    Email = dt.Rows[i]["Email"].ToString(),
+                    CPF = dt.Rows[i]["cpf_cnpj"].ToString(),
+                    Senha = dt.Rows[i]["Senha"].ToString(),
+                };
+                lista.Add(item);
+            }
+            return lista;
+        }
+    }
+}
